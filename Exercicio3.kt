5c429ad93b8ea4496425e9807585eeeb62bf03f8
Lucas-Tino <lucas.rosa131@etec.sp.gov.br>
2024-02-27 22:44:53 -0300

    Dias da semana

---
+++ Exercicio3.kt
@@ -0,0 +1,18 @@
+/* Código Original:
+ * fun main() {
+ *     println("Tuesday")
+ *     println("Thursday")
+ *     println("Wednesday")
+ *     println("Friday")
+ *     println("Monday")
+ * }
+ */
+
+// Código Corrigido:
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
