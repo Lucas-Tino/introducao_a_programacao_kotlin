b13733e39ef5b496c833380eb8fbc408a113364d
Lucas-Tino <lucas.rosa131@etec.sp.gov.br>
2024-02-27 22:50:34 -0300

    Cloudy (nublado)

---
+++ Exercicio6.kt
@@ -0,0 +1,12 @@
+/* Código original:
+ * fun main() {
+ *     println("Cloudy") println("Partly Cloudy") println("Windy")
+ * }
+ */
+
+// Código Corrigido:
+fun main() {
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+}
